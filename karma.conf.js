04712ac024308f717526c16b7b8e9ccb8d1911c5
Erik Barke <e@barke.nu>
2015-10-04 07:09:40 +0200

    tidied up to make jscs and jshint happy

--- karma.conf.js
+++ karma.conf.js
@@ -2,89 +2,80 @@
 // Generated on Wed Sep 16 2015 06:17:18 GMT+0200 (CEST)
 
 module.exports = function(config) {
-  config.set({
 
-    // base path that will be used to resolve all patterns (eg. files, exclude)
-    basePath: '',
+    'use strict';
 
+    config.set({
 
-    // frameworks to use
-    // available frameworks: https://npmjs.org/browse/keyword/karma-adapter
-    frameworks: ['jasmine'],
+        // base path that will be used to resolve all patterns (eg. files, exclude)
+        basePath: '',
 
+        // frameworks to use
+        // available frameworks: https://npmjs.org/browse/keyword/karma-adapter
+        frameworks: ['jasmine'],
 
-    // list of files / patterns to load in the browser
-    files: [
-        './src/client/js/lib/angular-1.4.6/angular.js',
-        './src/client/js/lib/angular-1.4.6/angular-mocks.js',
-        './src/client/js/app/**/*.module.js',
-        './src/client/js/app/**/*.js',
-        './src/client/js/app/**/*.html'
-    ],
+        // list of files / patterns to load in the browser
+        files: [
+            './src/client/js/lib/angular-1.4.6/angular.js',
+            './src/client/js/lib/angular-1.4.6/angular-mocks.js',
+            './src/client/js/app/**/*.module.js',
+            './src/client/js/app/**/*.js',
+            './src/client/js/app/**/*.html'
+        ],
 
+        // list of files to exclude
+        exclude: [
+        ],
 
-    // list of files to exclude
-    exclude: [
-    ],
+        // preprocess matching files before serving them to the browser
+        // available preprocessors: https://npmjs.org/browse/keyword/karma-preprocessor
+        preprocessors: {
+            './src/client/js/app/**/*.js': ['jshint', 'jscs'],
+            './src/client/js/app/**/!(*.spec).js': 'coverage',
+            './src/client/js/app/**/*.html': ['ng-html2js']
+        },
 
+        jshintPreprocessor: {
+            stopOnError: true
+        },
 
-    // preprocess matching files before serving them to the browser
-    // available preprocessors: https://npmjs.org/browse/keyword/karma-preprocessor
-    preprocessors: {
-        './src/client/js/app/**/*.js': ['jshint', 'jscs'],
-        './src/client/js/app/**/!(*.spec).js': 'coverage',
-        './src/client/js/app/**/*.html': ['ng-html2js']
-    },
+        jscsPreprocessor: {
+            configPath: '.jscsrc'
+        },
 
+        ngHtml2JsPreprocessor: {
 
-    jshintPreprocessor: {
-        stopOnError: true
-    },
+            stripPrefix: 'src/client/',
+            prependPrefix: 'apinterest/content/',
 
+            // the name of the Angular module to create
+            moduleName: 'html2js.unittest.templates'
+        },
 
-    jscsPreprocessor: {
-        configPath: '.jscsrc'
-    },
+        // test results reporter to use
+        // possible values: 'dots', 'progress'
+        // available reporters: https://npmjs.org/browse/keyword/karma-reporter
+        reporters: ['dots', 'coverage'],
 
-    ngHtml2JsPreprocessor: {
+        // web server port
+        port: 9876,
 
-        stripPrefix: "src/client/",
-        prependPrefix: "apinterest/content/",
+        // enable / disable colors in the output (reporters and logs)
+        colors: true,
 
-        // the name of the Angular module to create
-        moduleName: "html2js.unittest.templates"
-    },
+        // level of logging
+        // possible values: config.LOG_DISABLE || config.LOG_ERROR || config.LOG_WARN || config.LOG_INFO || config.LOG_DEBUG
+        logLevel: config.LOG_INFO,
 
-    // test results reporter to use
-    // possible values: 'dots', 'progress'
-    // available reporters: https://npmjs.org/browse/keyword/karma-reporter
-    reporters: ['dots', 'coverage'],
+        // enable / disable watching file and executing tests whenever any file changes
+        autoWatch: true,
 
-      
-    // web server port
-    port: 9876,
+        // start these browsers
+        // available browser launchers: https://npmjs.org/browse/keyword/karma-launcher
+        browsers: ['Chrome'],
 
-
-    // enable / disable colors in the output (reporters and logs)
-    colors: true,
-
-
-    // level of logging
-    // possible values: config.LOG_DISABLE || config.LOG_ERROR || config.LOG_WARN || config.LOG_INFO || config.LOG_DEBUG
-    logLevel: config.LOG_INFO,
-
-
-    // enable / disable watching file and executing tests whenever any file changes
-    autoWatch: true,
-
-
-    // start these browsers
-    // available browser launchers: https://npmjs.org/browse/keyword/karma-launcher
-    browsers: ['Chrome'],
-
-
-    // Continuous Integration mode
-    // if true, Karma captures browsers, runs the tests and exits
-    singleRun: false
-  })
-}
+        // Continuous Integration mode
+        // if true, Karma captures browsers, runs the tests and exits
+        singleRun: false
+    });
+};

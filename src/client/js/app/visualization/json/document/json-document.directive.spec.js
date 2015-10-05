276f83f2b1ec5810917caffaf1dc45d52dae4273
Erik Barke <e@barke.nu>
2015-10-05 06:38:06 +0200

    unit tests for json document directive

---
+++ src/client/js/app/visualization/json/document/json-document.directive.spec.js
@@ -0,0 +1,80 @@
+describe('jsonDocument', function() {
+
+    'use strict';
+
+    var mockJsonDocumentService,
+        $compile,
+        $rootScope,
+        element;
+
+    beforeEach(function() {
+
+        module('apinterest.json-editor');
+
+        mockJsonDocumentService = {
+            createHtml: function() {
+                return '<div>json document</div>';
+            }
+        };
+
+        module(function($provide) {
+
+            $provide.value('JsonDocumentService', mockJsonDocumentService);
+        });
+
+        inject(function($injector) {
+
+            $compile = $injector.get('$compile');
+            $rootScope = $injector.get('$rootScope');
+        });
+
+        $rootScope.name = 'name';
+        $rootScope.model = {
+            value: 'abc123'
+        };
+        $rootScope.validators = [];
+
+        compile();
+    });
+
+    it('should handle undefined model', function() {
+
+        $rootScope.model.value = undefined;
+
+        compile();
+
+        expect(element.find('div').html()).toBeUndefined();
+    });
+
+    it('should handle null model', function() {
+
+        $rootScope.model.value = undefined;
+
+        compile();
+
+        expect(element.find('div').html()).toBeUndefined();
+    });
+
+    it('should call json document service to create html', function() {
+
+        spyOn(mockJsonDocumentService, 'createHtml').and.callThrough();
+
+        compile();
+
+        expect(mockJsonDocumentService.createHtml).toHaveBeenCalledWith('abc123', 'name', []);
+    });
+
+    it('should compile element with html from json document service', function() {
+
+        compile();
+
+        expect(element.find('div').html()).toEqual('json document');
+    });
+
+    function compile() {
+
+        element = $compile('<div json-document name="name" model="model" validators="validators"></div>')($rootScope);
+
+        $rootScope.$digest();
+    }
+});

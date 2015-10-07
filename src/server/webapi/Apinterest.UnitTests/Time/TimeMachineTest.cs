35cb52f3071394a5168f79ff19d5ce98c5c58e72
Erik Barke <e@barke.nu>
2015-10-07 05:22:30 +0200

    unit tests for TimeMachine

---
+++ src/server/webapi/Apinterest.UnitTests/Time/TimeMachineTest.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using NUnit.Framework;
+using Apinterest.Time;
+
+namespace Apinterest.UnitTests.Time
+{
+    [TestFixture]
+    public class TimeMachineTest
+    {
+        private TimeMachine _timeMachine;
+
+        [SetUp]
+        public void SetUp()
+        {
+            _timeMachine = new TimeMachine();
+        }
+
+        [Test]
+        public void Should_Return_DateTime_With_Zero_Milliseconds()
+        {
+            Assert.That(_timeMachine.DateTime.Millisecond, Is.EqualTo(0));
+        }
+
+        [Test]
+        public void Should_Return_Time_With_Zero_Milliseconds()
+        {
+            Assert.That(_timeMachine.Time.Milliseconds, Is.EqualTo(0));
+        }
+    }
+}

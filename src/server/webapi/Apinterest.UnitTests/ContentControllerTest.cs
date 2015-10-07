27dc52a324bfcd7d6d850f90c1f14a8a7c4a4282
Erik Barke <e@barke.nu>
2015-10-07 06:36:43 +0200

    refactoring and unit tests for ContentController

---
+++ src/server/webapi/Apinterest.UnitTests/ContentControllerTest.cs
@@ -0,0 +1,126 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using Apinterest.Contracts;
+using Apinterest.Resources;
+using Moq;
+using Newtonsoft.Json.Converters;
+using Newtonsoft.Json.Serialization;
+using NUnit.Framework;
+
+namespace Apinterest.UnitTests
+{
+    [TestFixture]
+    public class ContentControllerTest
+    {
+        private ContentController _contentController;
+
+        private Mock<IRouteExplorerService> _mockRouteExplorerService;
+        private Mock<IResourceLookup> _mockResourceLookup;
+            
+        [SetUp]
+        public void SetUp()
+        {
+            _mockRouteExplorerService = new Mock<IRouteExplorerService>();
+
+            _mockRouteExplorerService
+                .Setup(e => e.GetRouteDescriptions())
+                .Returns(new List<RouteDescriptionContract>
+                {
+                    new RouteDescriptionContract
+                    {
+                        RelativePath = "some/path"
+                    }
+                });
+
+            _mockResourceLookup = new Mock<IResourceLookup>();
+
+            _mockResourceLookup
+                .Setup(r => r.GetString("index.html"))
+                .Returns("<tagsoup>hello</tagsoup>");
+
+            _mockResourceLookup
+                .Setup(r => r.GetString("app.js"))
+                .Returns("var x = 'y';");
+
+            _contentController = new ContentController(_mockRouteExplorerService.Object, _mockResourceLookup.Object);
+        }
+
+        [Test]
+        public void Should_Instantiate_With_Default_Constructor()
+        {
+            Assert.That(new ContentController(), Is.Not.Null);
+        }
+
+        [Test]
+        public void Should_Get_Index_File_Content()
+        {
+            var response = _contentController.GetIndex();
+
+            Assert.That(response.Content.ReadAsStringAsync().Result, Is.EqualTo("<tagsoup>hello</tagsoup>"));
+        }
+
+        [Test]
+        public void Should_Get_Index_File_Content_And_Call_Resource_Lookup()
+        {
+            _contentController.GetIndex();
+
+            _mockResourceLookup.Verify(r => r.GetString("index.html"));
+        }
+
+        [Test]
+        public void Should_Resolve_Index_File_Content_Type()
+        {
+            var response = _contentController.GetIndex();
+
+            Assert.That(response.Content.Headers.ContentType.ToString(), Is.EqualTo("text/html; charset=utf-8"));
+        }
+
+        [Test]
+        public void Should_Get_File_Content()
+        {
+            var response = _contentController.GetFile("app.js");
+
+            Assert.That(response.Content.ReadAsStringAsync().Result, Is.EqualTo("var x = 'y';"));
+        }
+
+        [Test]
+        public void Should_Get_File_Content_And_Call_Resource_Lookup()
+        {
+            _contentController.GetFile("app.js");
+
+            _mockResourceLookup.Verify(r => r.GetString("app.js"));
+        }
+
+        [Test]
+        public void Should_Resolve_File_Content_Type()
+        {
+            var response = _contentController.GetFile("app.js");
+
+            Assert.That(response.Content.Headers.ContentType.ToString(), Is.EqualTo("application/x-javascript; charset=utf-8"));
+        }
+
+        [Test]
+        public void Should_Get_Route_Descriptions()
+        {
+            var response = _contentController.GetRouteDescriptions();
+
+            Assert.That(response.Content.ElementAt(0).RelativePath, Is.EqualTo("some/path"));
+        }
+
+        [Test]
+        public void Should_Get_Route_Descriptions_And_Return_Json_Result_With_Lower_Camel_Case()
+        {
+            var response = _contentController.GetRouteDescriptions();
+
+            Assert.That(response.SerializerSettings.ContractResolver, Is.InstanceOf(typeof(CamelCasePropertyNamesContractResolver)));
+        }
+
+        [Test]
+        public void Should_Get_Route_Descriptions_And_Return_Json_Result_With_Enum_Names()
+        {
+            var response = _contentController.GetRouteDescriptions();
+
+            Assert.That(response.SerializerSettings.Converters[0], Is.InstanceOf(typeof(StringEnumConverter)));
+        }
+    }
+}
--- src/server/webapi/Apinterest/ContentController.cs
+++ src/server/webapi/Apinterest/ContentController.cs
@@ -1,9 +1,9 @@
 ﻿using System.Collections.Generic;
-using System.IO;
 using System.Net;
 using System.Net.Http;
 using System.Reflection;
 using System.Text;
+using System.Web;
 using System.Web.Http;
 using System.Web.Http.Description;
 using System.Web.Http.Results;
@@ -23,10 +23,14 @@ namespace Apinterest
     [ApiExplorerSettings(IgnoreApi = true)]
     public class ContentController : ApiController
     {
-        private readonly Dictionary<string, string> _mediaTypeMappings;
-        private readonly IRouteExplorerService _apiExplorerService;
+        private static readonly JsonSerializerSettings JsonSerializerSettings = new JsonSerializerSettings()
+        {
+            ContractResolver = new CamelCasePropertyNamesContractResolver(),
+            Converters = new JsonConverter[] { new StringEnumConverter() }
+        };
+
+        private readonly IRouteExplorerService _routeExplorerService;
         private readonly IResourceLookup _resourceLookup;
-        private readonly JsonSerializerSettings _jsonSerializerSettings;
 
         public ContentController()
         {
@@ -35,28 +39,14 @@ namespace Apinterest
             var logger = LogManager.GetLogger("Apinterest");
             var sampleFactory = new SampleFactory(validatorFactory, timeMachine, logger);
 
-            _apiExplorerService = new RouteExplorerService(GlobalConfiguration.Configuration.Services.GetApiExplorer(), sampleFactory);
+            _routeExplorerService = new RouteExplorerService(GlobalConfiguration.Configuration.Services.GetApiExplorer(), sampleFactory);
             _resourceLookup = new ResourceLookup(Assembly.GetExecutingAssembly());
-
-            _mediaTypeMappings = new Dictionary<string, string>
-            {
-                { ".css", "text/css" },
-                { ".html", "text/html" },
-                { ".js", "application/javascript" }
-            };
-
-            _jsonSerializerSettings = new JsonSerializerSettings()
-            {
-                ContractResolver = new CamelCasePropertyNamesContractResolver(),
-                Converters = new JsonConverter[] { new StringEnumConverter() }
-            };
         }
 
-        [HttpGet]
-        [Route("apinterest/route-descriptions")]
-        public JsonResult<IEnumerable<RouteDescriptionContract>> GetRouteDescriptions()
+        public ContentController(IRouteExplorerService routeExplorerService, IResourceLookup resourceLookup)
         {
-            return Json(_apiExplorerService.GetRouteDescriptions(), _jsonSerializerSettings);
+            _routeExplorerService = routeExplorerService;
+            _resourceLookup = resourceLookup;
         }
 
         [HttpGet]
@@ -73,26 +63,21 @@ namespace Apinterest
             return CreateStringResponse(file);
         }
 
+        [HttpGet]
+        [Route("apinterest/route-descriptions")]
+        public JsonResult<IEnumerable<RouteDescriptionContract>> GetRouteDescriptions()
+        {
+            return Json(_routeExplorerService.GetRouteDescriptions(), JsonSerializerSettings);
+        }
+
         private HttpResponseMessage CreateStringResponse(string file)
         {
-            var mediaType = ResolveMediaType(file);
+            var mediaType = MimeMapping.GetMimeMapping(file);
 
             return new HttpResponseMessage(HttpStatusCode.OK)
             {
                 Content = new StringContent(_resourceLookup.GetString(file), Encoding.UTF8, mediaType)
             };
-        }
-
-        private string ResolveMediaType(string file)
-        {
-            var extension = Path.GetExtension(file);
-
-            if (extension != null && _mediaTypeMappings.ContainsKey(extension))
-            {
-                return _mediaTypeMappings[extension];
-            }
-
-            return "";
         }
     }
 }

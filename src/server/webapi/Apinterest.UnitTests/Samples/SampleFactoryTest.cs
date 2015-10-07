d82539cae4997c25602e70945919364f5369dc1e
Erik Barke <e@barke.nu>
2015-10-07 05:07:13 +0200

    unit tests for SampleFactory

--- src/server/webapi/Apinterest.UnitTests/Samples/SampleFactoryTest.cs
+++ src/server/webapi/Apinterest.UnitTests/Samples/SampleFactoryTest.cs
@@ -121,5 +121,21 @@ namespace Apinterest.UnitTests.Samples
             Assert.That(sample.Instance, Is.Not.EqualTo(Guid.Empty));
             Assert.That(sample.Instance is Guid?);
         }
+
+        [Test]
+        public void Should_Instantiate_Uri()
+        {
+            var sample = SampleFactory.CreateSample(typeof(Uri));
+
+            Assert.That(sample.Instance, Is.EqualTo(new Uri("http://host/path")));
+        }
+
+        [Test]
+        public void Should_Instantiate_And_Handle_Null()
+        {
+            var sample = SampleFactory.CreateSample(null);
+
+            Assert.That(sample, Is.Null);
+        }
     }
 }

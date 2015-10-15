f2774e97c21b90acbc2b59985ce649b431387099
Erik Barke <e@barke.nu>
2015-10-15 06:18:26 +0200

    unit tests for recent history service

--- src/client/js/app/recent-history/recent-history.service.spec.js
+++ src/client/js/app/recent-history/recent-history.service.spec.js
@@ -3,6 +3,7 @@ describe('recent-history', function() {
     'use strict';
 
     var recentHistory,
+        mockRequestRunnerModel,
         mockAesCtr,
         mockLocalStorage,
         mockWindow;
@@ -10,6 +11,15 @@ describe('recent-history', function() {
     beforeEach(function() {
 
         module('apinterest.recent-history');
+
+        mockRequestRunnerModel = {
+            id: 'GETsome/path',
+            username: 'abc',
+            password: 'xyz',
+            parameters: [
+                { name: 'customer', value: 'Acme' }
+            ]
+        };
 
         mockAesCtr = {
             encrypt: function() {
@@ -23,12 +33,34 @@ describe('recent-history', function() {
         };
 
         mockLocalStorage = {
-            setItem: function() {
 
+            mockDatabase: {
+                'recent@GETsome/path': JSON.stringify([
+                    {
+                        date: '2015-09-01T23:59:59', username: 'abc', password: 'encrypted value', parameters: [
+                            { name: 'customer', value: 'Acme' },
+                            { name: 'customer', value: 'BigCorp' }
+                        ]
+                    },
+                    { date: '2015-09-01T23:59:59', username: 'abc', password: 'encrypted value', parameters: [] },
+                    { date: '2015-09-01T23:59:59', username: 'abc', password: 'encrypted value', parameters: [] },
+                    { date: '2015-09-01T23:59:59', username: 'abc', password: 'encrypted value', parameters: [] },
+                    { date: '2015-09-01T23:59:59', username: 'abc', password: 'encrypted value', parameters: [] },
+                    { date: '2015-09-01T23:59:59', username: 'abc', password: 'encrypted value', parameters: [] },
+                    { date: '2015-09-01T23:59:59', username: 'abc', password: 'encrypted value', parameters: [] },
+                    { date: '2015-09-01T23:59:59', username: 'abc', password: 'encrypted value', parameters: [] },
+                    { date: '2015-09-01T23:59:59', username: 'abc', password: 'encrypted value', parameters: [] },
+                    { date: '2015-09-01T23:59:59', username: 'abc', password: 'xencrypted value', parameters: [] }
+                ])
             },
-            getItem: function() {
 
-                return '{"username":"user","password":"encrypted value"}';
+            setItem: function(key, value) {
+
+                mockLocalStorage.mockDatabase[key] = value;
+            },
+            getItem: function(key) {
+
+                return mockLocalStorage.mockDatabase[key] || null;
             }
         };
 
@@ -52,72 +84,114 @@ describe('recent-history', function() {
 
         spyOn(mockAesCtr, 'encrypt').and.callThrough();
         spyOn(mockAesCtr, 'decrypt').and.callThrough();
-        spyOn(mockLocalStorage, 'setItem').and.callThrough();
         spyOn(mockLocalStorage, 'getItem').and.callThrough();
     });
 
-    /*
-    it('should encrypt user password', function() {
+    it('should save request runner model and get recent history list from local storage', function() {
 
-        storageService.setUserCredentials('user', 'pass', 'some/path');
+        recentHistory.save(mockRequestRunnerModel);
 
-        expect(mockAesCtr.encrypt).toHaveBeenCalledWith('pass', 'self-righteous cheese wheel banjo', 256);
+        expect(mockLocalStorage.getItem).toHaveBeenCalledWith('recent@GETsome/path');
     });
 
-    it('should set user credentials', function() {
+    it('should save request runner model and save to local storage, username', function() {
 
-        storageService.setUserCredentials('user', 'pass', 'some/path');
+        recentHistory.save(mockRequestRunnerModel);
 
-        expect(mockLocalStorage.setItem).toHaveBeenCalledWith('credentials@some/path', '{"username":"user","password":"encrypted value"}');
+        var stored = JSON.parse(mockLocalStorage.mockDatabase['recent@GETsome/path']);
+
+        expect(stored[0].username).toEqual('abc');
     });
 
-    it('should get user credentials', function() {
+    it('should save request runner model and save to local storage, password', function() {
 
-        storageService.getUserCredentials('some/path');
+        recentHistory.save(mockRequestRunnerModel);
 
-        expect(mockLocalStorage.getItem).toHaveBeenCalledWith('credentials@some/path');
+        var stored = JSON.parse(mockLocalStorage.mockDatabase['recent@GETsome/path']);
+
+        expect(stored[0].password).toEqual('encrypted value');
     });
 
-    it('should decrypt user password', function() {
+    it('should save request runner model and save to local storage, password', function() {
 
-        storageService.getUserCredentials('some/path');
+        recentHistory.save(mockRequestRunnerModel);
+
+        expect(mockAesCtr.encrypt).toHaveBeenCalledWith('xyz', 'self-righteous cheese wheel banjo', 256);
+    });
+
+    it('should save request runner model and save to local storage, parameters', function() {
+
+        recentHistory.save(mockRequestRunnerModel);
+
+        var stored = JSON.parse(mockLocalStorage.mockDatabase['recent@GETsome/path']);
+
+        expect(stored[0].parameters[0]).toEqual({ name: 'customer', value: 'Acme' });
+    });
+
+    it('should save request runner model and truncate history list exceeding max size', function() {
+
+        recentHistory.save(mockRequestRunnerModel);
+
+        var stored = JSON.parse(mockLocalStorage.mockDatabase['recent@GETsome/path']);
+
+        expect(stored.length).toEqual(10);
+    });
+
+    it('should save request runner model and keep history list under max size', function() {
+
+        mockLocalStorage.mockDatabase = {
+            'recent@GETsome/path': JSON.stringify([
+                { name: 'customer', value: 'BigCorp' }
+            ])
+        };
+
+        recentHistory.save(mockRequestRunnerModel);
+
+        var stored = JSON.parse(mockLocalStorage.mockDatabase['recent@GETsome/path']);
+
+        expect(stored.length).toEqual(2);
+    });
+
+    it('should get recent history list from local storage', function() {
+
+        recentHistory.get(mockRequestRunnerModel.id);
+
+        expect(mockLocalStorage.getItem).toHaveBeenCalledWith('recent@GETsome/path');
+    });
+
+    it('should get recent history list from local storage and handle non existing key', function() {
+
+        var recentHistoryList = recentHistory.get('does-not-exist');
+
+        expect(recentHistoryList.length).toEqual(0);
+    });
+
+    it('should get recent history list and set username', function() {
+
+        var recentHistoryList = recentHistory.get(mockRequestRunnerModel.id);
+
+        expect(recentHistoryList[0].username).toEqual('abc');
+    });
+
+    it('should get recent history list and decrypt password', function() {
+
+        recentHistory.get(mockRequestRunnerModel.id);
 
         expect(mockAesCtr.decrypt).toHaveBeenCalledWith('encrypted value', 'self-righteous cheese wheel banjo', 256);
+        expect(mockAesCtr.decrypt.calls.count()).toEqual(10);
     });
 
-    it('should get credentials for existing path, username', function() {
+    it('should get recent history list and set password', function() {
 
-        var credentials = storageService.getUserCredentials('some/path');
+        var recentHistoryList = recentHistory.get(mockRequestRunnerModel.id);
 
-        expect(credentials.username).toEqual('user');
+        expect(recentHistoryList[0].password).toEqual('decrypted value');
     });
 
-    it('should get credentials for existing path, password', function() {
+    it('should get recent history list and set parameters', function() {
 
-        var credentials = storageService.getUserCredentials('some/path');
+        var recentHistoryList = recentHistory.get(mockRequestRunnerModel.id);
 
-        expect(credentials.password).toEqual('decrypted value');
+        expect(recentHistoryList[0].parameters[0].value).toEqual('Acme');
     });
-
-    it('should handle non existing path, username', function() {
-
-        mockLocalStorage.getItem = function() {
-            return null;
-        };
-
-        var credentials = storageService.getUserCredentials('some/path');
-
-        expect(credentials.username).toEqual('');
-    });
-
-    it('should handle non existing path, password', function() {
-
-        mockLocalStorage.getItem = function() {
-            return null;
-        };
-
-        var credentials = storageService.getUserCredentials('some/path');
-
-        expect(credentials.password).toEqual('');
-    });*/
 });

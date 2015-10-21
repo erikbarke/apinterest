a2654ceed6fd47be5adfcfd4ec859285a6b0048a
Erik Barke <e@barke.nu>
2015-10-21 09:07:30 +0200

    added html reporter for jasmine

--- karma.conf.js
+++ karma.conf.js
@@ -53,7 +53,7 @@ module.exports = function(config) {
         // test results reporter to use
         // possible values: 'dots', 'progress'
         // available reporters: https://npmjs.org/browse/keyword/karma-reporter
-        reporters: ['dots', 'coverage'],
+        reporters: ['dots', 'coverage', 'html'],
 
         // web server port
         port: 9876,
